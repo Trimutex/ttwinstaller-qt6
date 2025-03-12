8fc1c484e2fa06bb5c30e4bc22ccc5c8051cee72
Trianta <56975502+Trimutex@users.noreply.github.com>
2025-03-12 02:39:48 -0500

    [WIP] Phase-3: Reintroduce copyright blocks before proceeding

--- src/Installer/Patching/Murmur/Murmur128UnmanagedX64.cpp
+++ src/Installer/Patching/Murmur/Murmur128UnmanagedX64.cpp
@@ -1,4 +1,19 @@
-﻿#include "Murmur128UnmanagedX64.hpp"
+﻿/*  Copyright 2012 Darren Kopp
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///    http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+*/
+
+#include "Murmur128UnmanagedX64.hpp"
 
 namespace TaleOfTwoWastelandsPatchingMurmur {
     Murmur128UnmanagedX64::Murmur128UnmanagedX64(unsigned seed = 0) : base(seed) {
